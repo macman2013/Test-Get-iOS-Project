2d9a835857aeb289d4dbb84c3e4cf4071c3bee6c
Dale M Keith <dmkeith@Dales-MacBook-Pro.local>
2015-06-07 16:23:23 -0400

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Dale M Keith on 6/7/15.
+//  Copyright (c) 2015 Dale M Keith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Dale M Keith on 6/7/15.
+//  Copyright (c) 2015 Dale M Keith. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
